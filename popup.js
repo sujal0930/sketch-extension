95d99c43b1985c3a47ab9dd89e0e164a78861ed1
Sujal <sujalagrawal069@gmail.com>
2022-03-17 00:18:17 +0530

    logic for saving strokes , on current page (restoring 101)

--- popup.js
+++ popup.js
@@ -16,3 +16,12 @@
             });
         });
     }
+
+    restore = document.getElementById("restore").onclick = function(){
+        console.log("restore");
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {action: "restore"}, function(response) {
+                console.log("response");
+            });
+        });
+    }
--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-
+const history=[]
 function setup(){
     document.body.style['userSelect'] = 'none';
     // console.log("setup");
@@ -6,18 +6,26 @@ function setup(){
     windowWidth = document.body.clientWidth;
     let c = createCanvas(windowWidth, windowHeight);
     c.style("pointer-events", "none");
+    c.style("z-index", 1000000000000000);
     c.position(0,0);    
 }
-
+    
 col=[110,110,110];
 function draw(){    
     chrome.runtime.onMessage.addListener(
         function(request,sender,snedResponse){
             if(request["action"] === "clear"){
                 clear();
+                // history=[];
             }
             if(request["action"] === "color"){
                 col=[Math.floor(Math.random()*255),Math.floor(Math.random()*255),Math.floor(Math.random()*255)];
+            }
+            if(request["action"] === "restore"){ 
+                for(ele of history){
+                    stroke(ele.rgb.r,ele.rgb.g,ele.rgb.b);
+                    line(ele.x,ele.y,ele.px,ele.py);
+                }
             }
         }
     );
@@ -26,6 +34,17 @@ function draw(){
     strokeWeight(3);
     if(mouseIsPressed){
         line(mouseX, mouseY, pmouseX, pmouseY);
+        history.push({
+            x:mouseX,
+            y:mouseY,
+            px:pmouseX,
+            py:pmouseY,
+            rgb:{
+                r:col[0],
+                g:col[1],
+                b:col[2]
+            }
+        });
     }
 
 }
