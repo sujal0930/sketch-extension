d520f8e3f83ec9b86525ec79b2576c5e4b496e18
Sujal <sujalagrawal069@gmail.com>
2022-03-23 20:56:24 +0530

    adding css to buttons

--- popup.js
+++ popup.js
@@ -1,36 +1,33 @@
 
-    userinput = document.getElementById("clear").onclick = function(){
-        console.log("clearCliked");
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {action: "clear"}, function(response) {
-                console.log("response");
-            });
-        });
-    }
-    
-    colorchange = document.getElementById("color").onclick = function(){
-        console.log("colorChange");
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {action: "color"}, function(response) {
-                console.log("response");
-            });
-        });
-    }
+userinput = document.getElementById("clear").onclick = function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {action: "clear"});
+    });
+}
 
-    restore = document.getElementById("restore").onclick = function(){
-        console.log("restore");
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {action: "restore"}, function(response) {
-                console.log("response");
-            });
-        });
-    }
+colorchange = document.getElementById("color").onclick = function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {action: "color"});
+    });
+}
 
-    save = document.getElementById("save").onclick = function(){
-        console.log("save");
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {action: "save"}, function(response) {
-                console.log("response");
-            });
-        });
-    }
+restore = document.getElementById("restore").onclick = function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {action: "restore"});
+    });
+}
+
+save = document.getElementById("save").onclick = function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {action: "save"});
+    });
+}
+
+let isWorking = document.getElementById("isWorking").checked;
+if(isWorking){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {action: "isWorking"});
+    });
+}
+
+
--- sketch.js
+++ sketch.js
@@ -1,70 +1,67 @@
+// Array for local storage.
 const history = []
 
+// url for storing in key value pair.
 var url_key = window.location.href;
-// console.log();
-// console.log(url_key)
 
 function setup() {
     document.body.style['userSelect'] = 'none';
     windowHeight = document.body.clientHeight;
     windowWidth = document.body.clientWidth;
+
     let c = createCanvas(windowWidth, windowHeight);
+    
     init_color = [110, 110, 110];
-    c.style("z-index", 1000000000000000);
+    c.style("z-index", 100);
     c.style("pointer-events", "none");
     c.position(0, 0);
 
 
     chrome.runtime.onMessage.addListener(
-        function (request, sender, snedResponse) {
+        function (request) {
             if (request["action"] === "clear") {
-                // console.log("clear");
                 clear();
-                // chrome.storage.sync.clear();
-                }
+            }
             if (request["action"] === "color") {
                 init_color = [Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255)];
             }
             if (request["action"] === "restore") {
+                
                 chrome.storage.sync.get(`${url_key}`, function (result) {
-                    // console.log(result);
+                    
                     if (result[url_key] != undefined) {
-                        // console.log(result);
+                        
                         temp = result[url_key];
+
                         for (ele of temp) {
-                            
                             stroke(ele.rgb.r, ele.rgb.g, ele.rgb.b);
                             line(ele.x, ele.y, ele.px, ele.py);
                         }
                     }
-                    else{
-                        console.log("No history");
+                    else {
+                        alert("No history");
                     }
 
                 });
             }
             if (request["action"] === "save") {
-                // console.log("Ye set hora",url_key)
-                    // chrome.storage.sync.clear();
-                    chrome.storage.sync.set({[url_key]: history}, function () {
-                    //    console.log("saved");
-                    });
-                }
-            });
+                chrome.storage.sync.set({ [url_key]: history });
+            }
+        });
 }
 
 function draw() {
-  
+
     stroke(init_color[0], init_color[1], init_color[2]);
     strokeWeight(3);
     if (mouseIsPressed) {
         line(mouseX, mouseY, pmouseX, pmouseY);
         history.push({
-            x: mouseX,
-            y: mouseY,
-            px: pmouseX,
-            py: pmouseY,
-            rgb: {
+            x : mouseX,
+            y : mouseY,
+            px : pmouseX,
+            py : pmouseY,
+            rgb : {
                 r: init_color[0],
                 g: init_color[1],
                 b: init_color[2]
